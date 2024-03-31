ea099b5b7a46cb334e1f7c8ad63bc716a0ab0ba8
Zinnurbek Ahrorov <a.zinnurbek@gmail.com>
2024-04-01 00:07:40 +0500

    sdf

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ document.getElementById('fetchDataBtn').addEventListener('click', fetchData);
 
 function fetchData() {
     // Fetch data from backend API and update the UI
-    fetch('http://localhost:8080/api/employees')
+    fetch('https://f03a-195-158-9-110.ngrok-free.app/api/employees')
         .then(response => response.json())
         .then(data => {
             document.getElementById('dataContainer').innerHTML = JSON.stringify(data);
